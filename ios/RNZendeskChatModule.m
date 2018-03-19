6b6106cba20442dc06202f5aa2bf58813aeda034
Francisco Javier Trujillo Mata <francisco.trujillo@adddisongloballtd.com>
2018-03-19 10:55:55 +0100

    Remove not needed import and use constants for the keys

--- ios/RNZendeskChatModule.m
+++ ios/RNZendeskChatModule.m
@@ -9,22 +9,33 @@
 #import "RNZendeskChatModule.h"
 #import <ZDCChat/ZDCChat.h>
 
+static NSString * const RNZendeskNameKey = @"name";
+static NSString * const RNZendeskEmailKey = @"email";
+static NSString * const RNZendeskPhoneKey = @"phone";
+static NSString * const RNZendeskShouldPersistKey = @"shouldPersist";
+static NSString * const RNZendeskDepartmentKey = @"departemnt";
+static NSString * const RNZendeskTagsKey = @"tags";
+static NSString * const RNZendeskEmailNotRequiredKey = @"emailNotRequired";
+static NSString * const RNZendeskPhoneNotRequiredKey = @"phoneNotRequired";
+static NSString * const RNZendeskDepartmentNotRequiredKey = @"departmentNotRequired";
+static NSString * const RNZendeskMessageNotRequiredKey = @"messageNotRequired";
+
 @implementation RNZendeskChatModule
 
 RCT_EXPORT_MODULE(RNZendeskChatModule);
 
 RCT_EXPORT_METHOD(setVisitorInfo:(NSDictionary *)options) {
   [ZDCChat updateVisitor:^(ZDCVisitorInfo *visitor) {
-    if (options[@"name"]) {
-      visitor.name = options[@"name"];
+    if (options[RNZendeskNameKey]) {
+      visitor.name = options[RNZendeskNameKey];
     }
-    if (options[@"email"]) {
-      visitor.email = options[@"email"];
+    if (options[RNZendeskEmailKey]) {
+      visitor.email = options[RNZendeskEmailKey];
     }
-    if (options[@"phone"]) {
-      visitor.phone = options[@"phone"];
+    if (options[RNZendeskPhoneKey]) {
+      visitor.phone = options[RNZendeskPhoneKey];
     }
-    visitor.shouldPersist = options[@"shouldPersist"] || NO;
+    visitor.shouldPersist = options[RNZendeskShouldPersistKey] || NO;
   }];
 }
 
@@ -33,17 +44,17 @@ RCT_EXPORT_METHOD(startChat:(NSDictionary *)options) {
 
   dispatch_sync(dispatch_get_main_queue(), ^{
     [ZDCChat startChat:^(ZDCConfig *config) {
-      if (options[@"department"]) {
-        config.department = options[@"department"];
+      if (options[RNZendeskDepartmentKey]) {
+        config.department = options[RNZendeskDepartmentKey];
       }
-      if (options[@"tags"]) {
-        config.tags = options[@"tags"];
+      if (options[RNZendeskTagsKey]) {
+        config.tags = options[RNZendeskTagsKey];
       }
-      config.preChatDataRequirements.name       = ZDCPreChatDataRequired;
-      config.preChatDataRequirements.email      = options[@"emailNotRequired"] ? ZDCPreChatDataNotRequired : ZDCPreChatDataRequired;
-      config.preChatDataRequirements.phone      = options[@"phoneNotRequired"] ? ZDCPreChatDataNotRequired : ZDCPreChatDataRequired;
-      config.preChatDataRequirements.department = options[@"departmentNotRequired"] ? ZDCPreChatDataNotRequired : ZDCPreChatDataRequiredEditable;
-      config.preChatDataRequirements.message    = options[@"messageNotRequired"] ? ZDCPreChatDataNotRequired : ZDCPreChatDataRequired;
+      config.preChatDataRequirements.name = ZDCPreChatDataRequired;
+      config.preChatDataRequirements.email = options[RNZendeskEmailNotRequiredKey] ? ZDCPreChatDataNotRequired : ZDCPreChatDataRequired;
+      config.preChatDataRequirements.phone = options[RNZendeskPhoneNotRequiredKey] ? ZDCPreChatDataNotRequired : ZDCPreChatDataRequired;
+      config.preChatDataRequirements.department = options[RNZendeskDepartmentNotRequiredKey] ? ZDCPreChatDataNotRequired : ZDCPreChatDataRequiredEditable;
+      config.preChatDataRequirements.message = options[RNZendeskMessageNotRequiredKey] ? ZDCPreChatDataNotRequired : ZDCPreChatDataRequired;
     }];
   });
 }
