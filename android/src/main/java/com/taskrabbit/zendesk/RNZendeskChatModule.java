4bc780234c96953d63b7c3eed09d1aaf3709834c
Felipe de Souza Paiva <spaiva.felipe@gmail.com>
2018-05-02 15:38:34 -0300

    Fix import

--- android/src/main/java/com/taskrabbit/zendesk/RNZendeskChatModule.java
+++ android/src/main/java/com/taskrabbit/zendesk/RNZendeskChatModule.java
@@ -11,6 +11,7 @@ import com.facebook.react.bridge.ReadableMap;
 import com.zopim.android.sdk.api.ZopimChat;
 import com.zopim.android.sdk.model.VisitorInfo;
 import com.zopim.android.sdk.prechat.ZopimChatActivity;
+import com.zopim.android.sdk.prechat.EmailTranscript;
 
 import java.lang.String;
 
